89dcde718fdac95062966c2d159bc7f618e1bf96
lkb <lalitkbehera@gmail.com>
2018-06-12 21:55:56 +0530

    020 Creating a New Deck

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,18 @@ import "fmt"
 // which is a slice of strings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Club"}
+	cardValue := []string{"Ace", "Two", "Three", "Four"}
+	for _, suit := range cardSuits {
+		for _, value := range cardValue {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 // Receiver function
 func (d deck) print() {
 	for i, card := range d {
